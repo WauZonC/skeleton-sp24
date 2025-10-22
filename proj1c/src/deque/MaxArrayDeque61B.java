55dd7d9ac91ff2800555ef70585df9ccbb65b958
WauZonC <2665234709@qq.com>
2025-10-22 22:20:30 +0800

    Project 1c Part 2 : Fixed

--- proj1c/src/deque/MaxArrayDeque61B.java
+++ proj1c/src/deque/MaxArrayDeque61B.java
@@ -8,5 +8,27 @@ public class MaxArrayDeque61B<T> extends ArrayDeque61B<T> {
     /**@param c a comparator for MaxArrayDeque so that it has order
      * */
     public MaxArrayDeque61B(Comparator<T> c) {
+        comparator = c;
+    }
+
+    public T max() {
+        if (size() == 0) {
+            return null;
+        }
+        T maxItem = get(0);
+        for (int i = 0; i < size(); i++) {
+            if (comparator.compare(get(i), maxItem) > 0) {
+                maxItem = get(i);
+            }
+        }
+        return maxItem;
+    }
+
+    public T max(Comparator<T> newComparator) {
+        Comparator<T> previousComparator = comparator;
+        comparator = newComparator;
+        T returnValue = max();
+        comparator = previousComparator;
+        return returnValue;
     }
 }
--- proj1c/tests/MaxArrayDeque61BTest.java
+++ proj1c/tests/MaxArrayDeque61BTest.java
@@ -21,4 +21,37 @@ public class MaxArrayDeque61BTest {
         mad.addFirst("fury road");
         assertThat(mad.max()).isEqualTo("fury road");
     }
+
+    @Test
+    @DisplayName("max")
+    public void maxTest() {
+        MaxArrayDeque61B<String> list = new MaxArrayDeque61B<>(new StringLengthComparator());
+        assertThat(list.max()).isEqualTo(null);
+        list.addLast("");
+        assertThat(list.max()).isEqualTo("");
+        list.addLast("123");
+        assertThat(list.max()).isEqualTo("123");
+        list.addLast("12");
+        assertThat(list.max()).isEqualTo("123");
+        list.removeLast();
+        list.removeLast();
+        assertThat(list.max()).isEqualTo("");
+    }
+
+    @Test
+    @DisplayName("max with comparator")
+    public void maxWithParameterTest() {
+        MaxArrayDeque61B<String> list = new MaxArrayDeque61B<>(new StringLengthComparator());
+        assertThat(list.max(Comparator.naturalOrder())).isEqualTo(null);
+        list.addLast("");
+        assertThat(list.max(Comparator.naturalOrder())).isEqualTo("");
+        list.addLast("acc");
+        assertThat(list.max(Comparator.naturalOrder())).isEqualTo("acc");
+        list.addLast("b");
+        assertThat(list.max(Comparator.naturalOrder())).isEqualTo("b");
+        assertThat(list.max()).isEqualTo("acc");
+        list.removeLast();
+        list.removeLast();
+        assertThat(list.max(Comparator.naturalOrder())).isEqualTo("");
+    }
 }
